24ce9e37871e63d79d84c6c22bee4d35b5990289
goodtrailer <aldenwu0@gmail.com>
2022-04-01 21:33:18 -0700

    Make problems box track scroll pane width

--- src/main/java/goodtrailer/quizgen/QuizPanel.java
+++ src/main/java/goodtrailer/quizgen/QuizPanel.java
@@ -1,10 +1,14 @@
 package goodtrailer.quizgen;
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.Scrollable;
 import javax.swing.SwingConstants;
 
 import goodtrailer.quizgen.problem.IProblem;
@@ -13,21 +17,15 @@ import goodtrailer.quizgen.problem.Result;
 
 public class QuizPanel extends JPanel
 {
-    public static final int SCROLL_INCREMENT = 5;
-
     private static final long serialVersionUID = 4126538549451781876L;
 
-    private Box problemsBox = new Box(BoxLayout.PAGE_AXIS);
+    private ScrollableBox problemsBox = new ScrollableBox(BoxLayout.PAGE_AXIS);
     private JScrollPane scrollPane = new JScrollPane(problemsBox);
     private IProblem[] problems;
-    
+
     public QuizPanel()
     {
-        scrollPane.getVerticalScrollBar().setUnitIncrement(SCROLL_INCREMENT);
-        scrollPane.getHorizontalScrollBar().setUnitIncrement(SCROLL_INCREMENT);
-
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        
         add(scrollPane);
     }
 
@@ -35,13 +33,13 @@ public class QuizPanel extends JPanel
     {
         problems = new IProblem[count];
         problemsBox.removeAll();
-        
+
         {
             var p = factory.get();
             problems[count - 1] = p;
             problemsBox.add(p.getRootComponent(), 0);
         }
-        
+
         for (int i = count - 2; i >= 0; i--)
         {
             var p = factory.get();
@@ -63,5 +61,47 @@ public class QuizPanel extends JPanel
     public int count()
     {
         return problems.length;
+    }
+
+    private class ScrollableBox extends Box implements Scrollable
+    {
+        public static final int SCROLL_INCREMENT = 20;
+        
+        private static final long serialVersionUID = 8642183969100983700L;
+        
+        public ScrollableBox(int axis)
+        {
+            super(axis);
+        }
+
+        @Override
+        public Dimension getPreferredScrollableViewportSize()
+        {
+            return getPreferredSize();
+        }
+
+        @Override
+        public int getScrollableUnitIncrement(Rectangle visible, int orientation, int direction)
+        {
+            return SCROLL_INCREMENT;
+        }
+
+        @Override
+        public int getScrollableBlockIncrement(Rectangle visible, int orientation, int direction)
+        {
+            return SCROLL_INCREMENT;
+        }
+
+        @Override
+        public boolean getScrollableTracksViewportWidth()
+        {
+            return true;
+        }
+
+        @Override
+        public boolean getScrollableTracksViewportHeight()
+        {
+            return false;
+        }
     }
 }
