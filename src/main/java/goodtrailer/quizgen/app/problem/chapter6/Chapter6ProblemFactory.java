01d58f369ebfeff4b5daa878d80fce61c203b806
goodtrailer <aldenwu0@gmail.com>
2022-04-08 23:30:36 -0700

    chapter6: Tweak weights

--- src/main/java/goodtrailer/quizgen/app/problem/chapter6/Chapter6ProblemFactory.java
+++ src/main/java/goodtrailer/quizgen/app/problem/chapter6/Chapter6ProblemFactory.java
@@ -9,10 +9,10 @@ public class Chapter6ProblemFactory extends AbstractWeightedProblemFactory
     private static final List<WeightedFactory> weighted_factories = List.of(
             new WeightedFactory(ExponentialDomainProblem::new, 1),
             new WeightedFactory(ExponentialRangeProblem::new, 1),
-            new WeightedFactory(ExponentialTableBaseProblem::new, 1),
+            new WeightedFactory(ExponentialTableBaseProblem::new, 2),
             new WeightedFactory(ExponentialGrowthTypeProblem::new, 1),
             new WeightedFactory(ExponentialRateOfChangeProblem::new, 1),
-            new WeightedFactory(ExponentialIntersectionProblem::new, 3));
+            new WeightedFactory(ExponentialIntersectionProblem::new, 2));
 
     @Override
     protected List<WeightedFactory> getWeightedFactories()
