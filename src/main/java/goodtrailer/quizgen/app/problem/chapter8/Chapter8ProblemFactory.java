a8fde391293963eb363ad9527783c4785e562c59
Alden Wu <aldenwu0@gmail.com>
2022-11-01 01:15:39 -0700

    chapter8: Add complementary angle problem

--- src/main/java/goodtrailer/quizgen/app/problem/chapter8/Chapter8ProblemFactory.java
+++ src/main/java/goodtrailer/quizgen/app/problem/chapter8/Chapter8ProblemFactory.java
@@ -7,11 +7,10 @@ import goodtrailer.quizgen.problem.AbstractWeightedProblemFactory;
 public class Chapter8ProblemFactory extends AbstractWeightedProblemFactory
 {
     private static final List<WeightedFactory> weighted_factories = List.of(
+            new WeightedFactory(ComplementaryAngleProblem::new, 1),
             new WeightedFactory(SupplementaryAngleProblem::new, 1));
-    
+
     @Override
     protected List<WeightedFactory> getWeightedFactories()
-    {
-        return weighted_factories;
-    }
+    { return weighted_factories; }
 }
